dc257e658c0e1ad2ea90630695a5ad166b56dd58
ingrptr-lt <ingrptr@gmail.com>
2026-01-26 01:10:25 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// =========================================================================
+//         CONFIGURATION (EDIT THIS FILE ONLY)
+// =========================================================================
+
+const GITHUB_TOKEN = "ghp_YOUR_GITHUB_PAT_HERE"; // <--- PASTE YOUR TOKEN HERE
